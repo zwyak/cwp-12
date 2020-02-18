8228879d1819158e78a733c8b3ef9fb047938eca
zwyak <vlad.senyuk97@gmail.com>
2020-02-18 11:37:30 +0300

    Defined weapon model

--- models/weapon.js
+++ models/weapon.js
@@ -1,5 +1,14 @@
 module.exports = (Sequelize, sequelize) => {
   return sequelize.define('weapons', {
-    // TODO: описание полей
+    id:{
+      type: Sequelize.INTEGER,
+      autoIncrement: true
+    },
+    name: {
+      type: Sequelize.STRING
+    },
+    dps: {
+      type: Sequelize.INTEGER
+    }
   });
 };
