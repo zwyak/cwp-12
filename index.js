de15219e3594c12cc7a644274b682e5f81044f92
zwyak <vlad.senyuk97@gmail.com>
2020-02-18 17:36:05 +0300

    Fixed errors

--- index.js
+++ index.js
@@ -3,14 +3,3 @@ const Sequelize = require('sequelize');
 const config = require('./config.json');
 
 const db = require('./models')(Sequelize, config);
-
-db.pizzas.create({
-    name: "Peperoni",
-    description: "Double cheese",
-    calories: 500
-  }).then(res=>{
-  }).catch(err=>console.log(err));
-
-db.pizzas.findAll({raw:true}).then(pizzas=>{
-    console.log(pizzas);
-  }).catch(err=>console.log(err));
