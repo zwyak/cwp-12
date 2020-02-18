1911ad63b3300485cc16fb21d63937efd13e7754
zwyak <vlad.senyuk97@gmail.com>
2020-02-18 11:36:28 +0300

    Defined pizza model

--- models/pizza.js
+++ models/pizza.js
@@ -1,5 +1,17 @@
 module.exports = (Sequelize, sequelize) => {
   return sequelize.define('pizzas', {
-    // TODO: описание полей
+  id:{
+    type: Sequelize.INTEGER,
+    autoIncrement: true
+  },
+  name: {
+    type: Sequelize.STRING
+  },
+  description: {
+    type: Sequelize.STRING
+  },
+  calories: {
+    type: Sequelize.DOUBLE
+  }
   });
 };
